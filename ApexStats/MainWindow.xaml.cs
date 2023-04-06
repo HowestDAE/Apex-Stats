ecf69abb4d884210dc0cd7c65c8cb7ec3d843762
Jef Belmans <belmansjef@gmail.com>
2023-04-06 20:56:01 +0200

    NuGet packages and Model added
    Added Newtonsoft JSON NuGet package.
    Added CommunityToolkit.MVVM NuGet package.
    Wrote Model class for the PlayerStatistics (contains every info about a player and the stats of their account and legends)

--- ApexStats/MainWindow.xaml.cs
+++ ApexStats/MainWindow.xaml.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using ApexStats.Model;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +29,20 @@ namespace ApexStats
         public MainWindow()
         {
             InitializeComponent();
+
+            PlayerStatistics stats;
+
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "ApexStats.Resources.Data.localdata.json";
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    string json = reader.ReadToEnd();
+                    stats = JsonConvert.DeserializeObject<JObject>(json).SelectToken("data").ToObject<PlayerStatistics>();
+                }
+            }
         }
     }
 }
---
+++ ApexStats/Model/PlayerStatistics.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace ApexStats.Model
+{
+    internal class PlayerStatistics
+    {
+        [JsonProperty(PropertyName = "platformInfo")]
+        PlatformInfo PlatformInfo { get; set; }
+        [JsonProperty(PropertyName = "metadata")]
+        Metadata Metadata { get; set; }
+        [JsonProperty(PropertyName = "segments")]
+        List<Segment> Segments { get; set; }
+    }
+
+    public class PlatformInfo
+    {
+        [JsonProperty(PropertyName = "platformSlug")]
+        public string Platform { get; set; }
+        [JsonProperty(PropertyName = "platformUserIdentifier")]
+        public string UserName { get; set; }
+        [JsonProperty(PropertyName = "avatarUrl")]
+        public string AvatarURL { get; set; }
+    }
+
+    public class Metadata
+    {
+        [JsonProperty(PropertyName = "currentSeason")]
+        public int CurrentSeason { get; set; }
+        [JsonProperty(PropertyName = "activeLegend")]
+        public string ActiveLegend { get; set; }
+        [JsonProperty(PropertyName = "activeLegendName")]
+        public string ActiveLegendName { get; set; }
+    }
+
+    public class Segment
+    {
+        [JsonProperty(PropertyName = "type")]
+        public string Type { get; set; }
+        [JsonProperty(PropertyName = "attributes")]
+        public Attributes Attributes { get; set; }
+        [JsonProperty(PropertyName = "metadata")]
+        public SegmentMetadata Metadata { get; set; }
+        [JsonProperty(PropertyName = "stats")]
+       public Dictionary<string, Stat> Statistics { get; set; }
+    }
+
+    public class Attributes
+    {
+        public string id { get; set; }
+    }
+
+    public class SegmentMetadata
+    {
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+        [JsonProperty(PropertyName = "imageUrl")]
+        public string ImageURL { get; set; }
+        [JsonProperty(PropertyName = "bgImageUrl")]
+        public string BgImageURL { get; set; }
+        [JsonProperty(PropertyName = "portraitImageUrl")]
+        public string PortraitImageURL { get; set; }
+        [JsonProperty(PropertyName = "legendColor")]
+        public string LegendColor { get; set; }
+    }
+
+    public class Stat
+    {
+        [JsonProperty(PropertyName = "percentile")]
+        public float? Percentile { get; set; }
+        [JsonProperty(PropertyName = "displayName")]
+        public string DisplayName { get; set; }
+        [JsonProperty(PropertyName = "metadata")]
+        public StatMetadata Metadata { get; set; }
+        [JsonProperty(PropertyName = "value")]
+        public int Value { get; set; }
+        [JsonProperty(PropertyName = "displayValue")]
+        public string DisplayValue { get; set; }
+    }
+
+    public class StatMetadata
+    {
+        [JsonProperty(PropertyName = "iconUrl")]
+        public string IconURL { get; set; }
+        [JsonProperty(PropertyName = "rankName")]
+        public string RankName { get; set; }
+    }
+}
