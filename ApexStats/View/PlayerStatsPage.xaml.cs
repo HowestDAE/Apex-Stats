e3a2f228925863461f95d1689c03e74a812b8457
Jef Belmans <belmansjef@gmail.com>
2023-04-11 20:32:44 +0200

    Reworked PlayerStats Page
    Reworked PlayerStats Page to be responsive.  ListView containing the Legend statistics now gets resized based on the window width, preventing items from falling of the screen.

--- ApexStats/View/PlayerStatsPage.xaml.cs
+++ ApexStats/View/PlayerStatsPage.xaml.cs
@@ -24,10 +24,5 @@ namespace ApexStats.View
         {
             InitializeComponent();
         }
-
-        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
     }
 }
